3b134c7fdf055c4cfaa354fa9566980c5ddf97ee
David <vidlew@outlook.com>
2018-05-31 10:15:05 -0300

    modified:   Planar.hs

--- Planar.hs
+++ Planar.hs
@@ -1,13 +1,15 @@
 {-# LANGUAGE GADTs, DataKinds, StandaloneDeriving, TypeFamilies, KindSignatures, TypeOperators #-}
 {-# LANGUAGE MultiParamTypeClasses, FunctionalDependencies, FlexibleInstances, FlexibleContexts #-}
-{-# LANGUAGE UndecidableInstances, ExistentialQuantification #-}
+{-# LANGUAGE UndecidableInstances, ExistentialQuantification, TypeInType #-}
 
 module Planar where
 
 import Nat
 
 -- A lambda skeleton is a linear lambda term with the variable names omitted.
--- Every lambda skeleton is the skeleton of exactly one planar linear lambda term.
+-- Every lambda skeleton is the skeleton of exactly one planar linear lambda term,
+-- where a linear lambda term is planar is there are no free variables between a
+-- lambda and the unique variable it binds.
 infixl 5 :.
 data Skeleton n where
     V    :: Skeleton (S Z) -- Placeholder for variable name
@@ -17,6 +19,10 @@ deriving instance Show (Skeleton n)
 
 data Colour = Blue | Red deriving (Show, Eq)
 
+-- A colouring of a lambda skeleton is a formal proof that it is neutral or normal.
+-- A lambda skeleton is normal if it contains no subterms of the form (L u):.v (β-redexes).
+-- A lambda skeleton u is neutral if, for every normal lambda term v, u:.v is normal.
+-- A lambda skeleton is neutral iff it can be coloured blue and normal if it can be coloured red.
 infixl 5 :&
 data ColouredSkeleton c n where
     BV   :: ColouredSkeleton Blue (S Z) -- v-rule
@@ -25,6 +31,8 @@ data ColouredSkeleton c n where
     RL   :: ColouredSkeleton Red (S n) -> ColouredSkeleton Red n
 deriving instance Show (ColouredSkeleton c n)
 
+-- The size of a coloured skeleton is the number of uses of the s-rule (every neutral
+-- skeleton is normal) in the proof that it is normal or neutral.
 size :: (Num a) => ColouredSkeleton c n -> a
 size BV     = 0
 size (u:&v) = (size u)+(size v)
@@ -50,6 +58,19 @@ norm :: Skeleton n -> Maybe (ColouredSkeleton Red n)
 norm (L u) = RL <$> norm u
 norm u     = RB <$> neut u
 
+type family (g :: List m Nat) :++ (d :: List n Nat) :: List (m:+n) Nat
+type instance E:++d      = d
+type instance (x:-g):++d = x:-(g:++d)
+
+-- Linear lambda terms.
+-- A linear lambda term is planar if it's equivalent to one that can be contructed without using T.
+data LinearLambda n (g :: List n Nat) where
+    LV   :: LinearLambda (S Z) (x:-E)
+    (:$) :: LinearLambda m g -> LinearLambda n d -> LinearLambda (m:+n) (g:++d)
+    Λ    :: LinearLambda (S n) (m:-g) -> LinearLambda n g
+    T    :: Permutation n -> LinearLambda n g -> LinearLambda n g
+deriving instance Show (LinearLambda n g)
+
 -- Motzkin trees. Essentially lambda skeletons without the linearity restriction.
 data Motzkin = P | B Motzkin Motzkin | U Motzkin deriving (Show, Eq)
 
@@ -62,7 +83,7 @@ instance Eq (Skeleton n) where
     u==v = (toMotzkin u) == (toMotzkin v)
 
 -- A lambda term is normal if it contains no beta-redexes.
--- A lambda term u is neutral if, for every lambda term v, u(v) is normal.
+-- A lambda term u is neutral if, for every normal lambda term v, u(v) is normal.
 isNeutral :: Skeleton n -> Bool
 isNeutral V = True
 isNeutral (u:.v) = isNeutral u && isNormal v
